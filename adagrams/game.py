bf911a6af73bd7f45ffe28ebc738e30b580e9d7f
Lia Gaetano <lgaetano19@gmail.com>
2021-09-21 16:39:18 -0700

    Create score_word()

--- adagrams/game.py
+++ adagrams/game.py
@@ -68,7 +68,40 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    score = 0
+
+    # list of lists
+    scores = [
+            ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S'], # 1
+            ['D', 'G'], # 2
+            ['B', 'C', 'M', 'P'], # 3
+            ['F', 'H', 'V', 'W', 'Y'], #4
+            ['K'], #5
+            ['J', 'X'],  #8
+            ['Q', 'Z'], #10
+    ]
+
+    for letter in word:
+        letter = letter.upper()
+        if letter in scores[0]:
+            score += 1
+        elif letter in scores[1]:
+            score += 2
+        elif letter in scores[2]:
+            score += 3
+        elif letter in scores[3]:
+            score += 4
+        elif letter in scores[4]:
+            score += 5
+        elif letter in scores[5]:
+            score += 8
+        elif letter in scores[6]:
+            score += 10
+
+    if 7 <= len(word) <= 10:
+        score += 8
+
+    return score
 
 def get_highest_word_score(word_list):
     pass
--- play_test.py
+++ play_test.py
@@ -1,96 +1,68 @@
-from random import choices
+# def score_word(word):
+
+#     # Create points collectio
+#     sample = {
+#         1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S'],
+#         2: ['D', 'G'],
+#         3: ['B', 'C', 'M', 'P'],
+#         4: ['F', 'H', 'V', 'W', 'Y'],
+#         5: ['K'],
+#         8: ['J', 'X'],
+#         10: ['Q', 'Z']
+#     }
+
+# #     return score
+# sample = {
+#         ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S'): 1,
+#         ('D', 'G'): 2,
+#         ('B', 'C', 'M', 'P'): 3,
+#         ('F', 'H', 'V', 'W', 'Y'): 4,
+#         ('K'): 5,
+#         ('J', 'X'): 8,
+#         ('Q', 'Z'): 10
+#     }
+
+# print(sample[1])
 
 
 
-LETTER_POOL = {
-    'A': 9, 
-    'B': 2, 
-    'C': 2, 
-    'D': 4, 
-    'E': 12, 
-    'F': 2, 
-    'G': 3, 
-    'H': 2, 
-    'I': 9, 
-    'J': 1, 
-    'K': 1, 
-    'L': 4, 
-    'M': 2, 
-    'N': 6, 
-    'O': 8, 
-    'P': 2, 
-    'Q': 1, 
-    'R': 6, 
-    'S': 4, 
-    'T': 6, 
-    'U': 4, 
-    'V': 2, 
-    'W': 2, 
-    'X': 1, 
-    'Y': 2, 
-    'Z': 1
-}
 
+def score_word(word):
 
+    score = 0
 
-# def draw_letters():
-#     '''
-#     Returns a list of 10 letters to user.  
-#     '''
+    # list of lists
+    scores = [
+            ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S'], # 1
+            ['D', 'G'], # 2
+            ['B', 'C', 'M', 'P'], # 3
+            ['F', 'H', 'V', 'W', 'Y'], #4
+            ['K'], #5
+            ['J', 'X'],  #8
+            ['Q', 'Z'], #10
+    ]
 
-    # letter_bank = {
-    #     'X': 7,
-    #     'Y': 2, 
-    #     'Z': 1        
-    # }
+    for letter in word:
+        letter = letter.upper()
+        if letter in scores[0]:
+            score += 1
+        elif letter in scores[1]:
+            score += 2
+        elif letter in scores[2]:
+            score += 3
+        elif letter in scores[3]:
+            score += 4
+        elif letter in scores[4]:
+            score += 5
+        elif letter in scores[5]:
+            score += 8
+        elif letter in scores[6]:
+            score += 10
 
-    # Randomly select a letter according to its weight in LETTER_POOL
-    # while len(letter_bank) < 10:
-        # letter_pool_keys = list(LETTER_POOL.keys())
-        # weighted_values = list(LETTER_POOL.values())
-        # # random.choices(population, weights, k) --> returns single letter list
-        # letter = choices(letter_pool_keys, weighted_values, k=1)
-        # letter = letter[0]
-    
-        # if letter in letter_bank:
-        #     # Ensure letter does not occur more frequently than in LETTER_POOL 
-        #     if letter_bank[letter] > LETTER_POOL[letter]:
-        #         pass
-        #     else:
-        #         # Increment letter frequency
-        #         letter_bank[letter] = letter_bank.get(letter, 0) + 1
-        # else:
-        #     # Add letter
-        #     letter_bank[letter] = letter_bank.get(letter, 1)
-    
-    
-    # Create list to display letters by frequency
-    # letter_bank_list = []
-    # for letter, freq in letter_bank.items():
-    #     for i in range(freq):
-    #         letter_bank_list.append(letter)
+    if 7 <= len(word) <= 10:
+        score += 8
 
-    # return letter_bank_list
+    return score
 
-    # return list(letter_bank.keys())
-
-# test_values = draw_letters()
-# print(test_values)
-
-
-# letters = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"]
-# word = "OOG"
-# #WAVE 2
-# def uses_available_letters(word,letter_bank):
-
-#     letter_bank_2 = letter_bank[:]
-
-#     for letter in word:
-#         if letter not in letter_bank_2:
-#             return False
-#         else:
-#             letter_bank_2.remove(letter)
-#     return True
-    
-# is_valid = uses_available_letters(word, letters)
-# print(is_valid)
+result = score_word("XXXXXXXXX")
+print(result)
